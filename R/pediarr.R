4783c5309a8a8f2179d8befacf3f7aa25379c3e0
chainsawriot <chainsawtiney@gmail.com>
2015-03-27 00:40:41 +0800

    Create the functions to query wikipedia API

--- R/pediarr.R
+++ R/pediarr.R
@@ -1,8 +1,31 @@
 require(httr)
-r <- GET("https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=Rice")
 
-content(r)$query$page[[1]]$extract
+pediaextract <- function(wikititle, lang = 'en') {
+    wikiapi <- paste0("https://", lang, ".wikipedia.org/w/api.php")
+    res <- GET(wikiapi, query = list(format = 'json', action = 'query', prop = 'extracts', titles = wikititle, exintro = '', explaintext = ''))
+    return(content(res)$query$page[[1]]$extract)
+}
 
-jx <- GET("https://ja.wikipedia.org/w/api.php", query = list(format = 'json', action = 'query', prop = 'extracts', titles = '米', exintro = '', explaintext = ''))
+pediaextracts("Hong_Kong")
+pediaextracts("Hong Kong") # smart enough to replace space with underscore
+pediaextracts("Hong_Kong", "es")
 
-content(jx)$query$page[[1]]$extract
+res <- GET("https://en.wikipedia.org/w/api.php", query = list(format = 'json', action = 'opensearch', search = 'Hong Kong', limit = '20', namespace = '0'))
+unlist(content(res)[[2]])
+unlist(content(res)[[3]])
+
+pediasearch <- function(searchstring, lang = 'en', extract = FALSE, limit = 20, namespace = '0') {
+    wikiapi <- paste0("https://", lang, ".wikipedia.org/w/api.php")
+    res <- GET(wikiapi, query = list(format = 'json', action = 'opensearch', search = searchstring, limit = limit, namespace = namespace))
+    if (extract) {
+        return(list(title = unlist(content(res)[[2]]), extract = unlist(content(res)[[3]])))
+    } else {
+        return(unlist(content(res)[[2]]))
+    }
+}
+
+pediasearch("ukulele")
+ukuleleextracts <- sapply(pediasearch("ukulele"), pediaextract) # it is not a good practice
+pediasearch("ukulele", extract = TRUE)
+
+pediasearch("Python")
