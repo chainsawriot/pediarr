214b8c46f81c28867eb72ffae62283372cf206e8
chainsawriot <chainsawtiney@gmail.com>
2015-03-27 08:13:31 +0800

    Update READ.md and clean up

--- R/pediarr.R
+++ R/pediarr.R
@@ -9,14 +9,6 @@ pediaextract <- function(wikititle, lang = 'en') {
     return(content(res)$query$page[[1]]$extract)
 }
 
-#pediaextracts("Hong_Kong")
-#pediaextracts("Hong Kong") # smart enough to replace space with underscore
-#pediaextracts("Hong_Kong", "es")
-
-#res <- GET("https://en.wikipedia.org/w/api.php", query = list(format = 'json', action = 'opensearch', search = 'Hong Kong', limit = '20', namespace = '0'))
-#unlist(content(res)[[2]])
-#unlist(content(res)[[3]])
-
 pediasearch <- function(searchstring, lang = 'en', extract = FALSE, limit = 20, namespace = '0') {
     res <- GET(langapi(lang), query = list(format = 'json', action = 'opensearch', search = searchstring, limit = limit, namespace = namespace))
     if (extract) {
@@ -26,21 +18,7 @@ pediasearch <- function(searchstring, lang = 'en', extract = FALSE, limit = 20, 
     }
 }
 
-#pediasearch("ukulele")
-#ukuleleextracts <- sapply(pediasearch("ukulele"), pediaextract) # it is not a good practice
-#pediasearch("ukulele", extract = TRUE)
-
-#pediasearch("Python")
-#pediasearch("Python", namespace = "14") # search for categories
-#pediasearch("Dynamically typed programming languages", namespace = 14, limit = 1) # search for categories
-
-
 pediacategory <- function(wikicategory, lang = 'en', limit = 500, cmtype = 'page|subcat|file') {
     res <- GET(langapi(lang), query = list(format = 'json', action = 'query', list = 'categorymembers', cmtitle = wikicategory, cmtype = cmtype, cmlimit = limit, cmprop = 'title'))
     return(sapply(content(res)$query$categorymembers, function(x) x$title))
 }
-
-#pediacategory("Category:Dynamically typed programming languages")
-
-#alldyn <- sapply(pediacategory("Category:Dynamically typed programming languages", cmtype = 'page'), pediaextract)
-#saveRDS(alldyn, "~/alldyn.rds")
--- README.md
+++ README.md
@@ -4,5 +4,30 @@ R interface to Wikipedia API
 
 ## Features
 
-* search wikipedia
-* retrieve extracts or full text
+* Search Wikipedia
+
+```r
+pediasearch("ukulele")
+pediasearch("ukulele", extract = TRUE)
+
+pediasearch("Python")
+pediasearch("Python", namespace = "14") # search for categories
+pediasearch("Dynamically typed programming languages", namespace = 14, limit = 1)
+```
+
+* Retrieve extract of Wikipedia article (Full text in the future)
+
+```r
+pediaextracts("Hong_Kong")
+pediaextracts("Hong Kong") # smart enough to replace space with underscore
+pediaextracts("Hong_Kong", lang="es")
+
+ukuleleextracts <- sapply(pediasearch("ukulele"), pediaextract) # it is not a good practice
+```
+
+* List out members of Wikipedia category
+
+```r
+pediacategory("Category:Dynamically typed programming languages")
+pediacategory("Category:Dynamically typed programming languages", cmtype = 'page') # ignore subcat and files
+```
